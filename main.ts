ab8c952070a07089c738d7369c0debaba4403721
Ki Frankie <106165688+kifrankie0910@users.noreply.github.com>
2023-02-24 15:41:24 +0800

    fan

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
-OLED.init(128, 64)
-basic.forever(function () {
-    OLED.writeString("Temp" + smarthome.ReadTemperature(TMP36Type.TMP36_temperature_C, AnalogPin.P2))
-    OLED.writeStringNewLine("Light" + Environment.ReadLightIntensity(AnalogPin.P2))
+input.onButtonPressed(Button.A, function () {
+	
+})
+input.onButtonPressed(Button.B, function () {
+	
 })
